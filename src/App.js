1deed2b72de284453cde9b2caa26407d136d5a5a
ashaaut <ashaautade1998@gmail.com>
2019-08-09 23:57:15 -0500

    trying

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Footer from './footer';
+function App() {
+  return (
+    <div className="App">
+         <h1>HELLO ASHA</h1>
+         <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+function Footer(){
+  return(
+      <footer><h5>govt. college eng. karad</h5></footer>
+  );
+}
+export default Footer;
